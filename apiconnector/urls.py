de00db13046b1a7b262eef91e62916d34ac95336
gustavo saavedra <gustavo saavedra>
2016-08-13 13:44:17 -0500

    tarde

--- apiconnector/urls.py
+++ apiconnector/urls.py
@@ -1,24 +1,16 @@
-"""apiconnector URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
+from apps.user import urls as user_urls
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('apps.home.urls',  namespace='home')),
-    url(r'^api/', include('apps.api.urls',  namespace='api')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-]
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^account/', include(user_urls)),
+                  url(r"^account/", include("account.urls")),
+                  url(r'^', include('apps.home.urls', namespace='home')),
+                  url(r'^api/', include('apps.api.urls', namespace='api')),
+                  url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+                  url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
