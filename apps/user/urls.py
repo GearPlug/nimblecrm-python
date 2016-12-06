621f96d11521fdca63b33aae5187a38cc9eb985a
Miguel Ferrer <ingferrermiguel@gmail.com>
2016-12-06 16:46:37 -0400

    Consultar a la API segun spreadsheet y sheetId

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from apps.user.views import SignupView, LoginView, email_test
+from apps.user.views import SignupView, LoginView, email_test, async_spreadsheet_info, async_spreadsheet_values
 
 urlpatterns = [
     url(r"^signup/$", SignupView.as_view(), name="user_signup"),
     url(r"^login/$", LoginView.as_view(), name="account_login"),
     url(r"^test/$", email_test, name="test_login"),
+    url(r"^async/spreadsheet/info/(?P<id>.+)/$", async_spreadsheet_info, name="async_test"),
+    url(r"^async/spreadsheet/values/(?P<id>.+)/(?P<sheet_id>.+)/$", async_spreadsheet_values, name="async_test"),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,9 +1,12 @@
+import json
 import account.views
 import account.forms
 import apps.user.forms
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.template import loader, Context
 import httplib2
 from oauth2client import client
+from oauth2client.file import Storage
 from apiclient import discovery
 from django.conf import settings
 
@@ -41,23 +44,28 @@ def email_test(request):
         redirect_uri='http://localhost/account/test/')
     credentials = flow.step2_exchange(code)
     http_auth = credentials.authorize(httplib2.Http())
+
+    storage = Storage('credentials')
+    storage.put(credentials)
+
     drive_service = discovery.build('drive', 'v3', http_auth)
     files = drive_service.files().list().execute()
+
+    print(files)
+
     sheet_list = []
-    for f in files:
-        if f == 'files':
-            for i in files[f]:
-                sheet_list.append((i['id'], i['name']))
-        else:
-            print("%s: %s" % (f, files[f]))
+    for f in files['files']:
+        print(f)
+        if 'mimeType' in f and f['mimeType'] == 'application/vnd.google-apps.spreadsheet':
+            sheet_list.append((f['id'], f['name']))
     print(sheet_list)
     print(sheet_list[0])
     spreadsheet_id = sheet_list[0][0]
     sheets_service = discovery.build('sheets', 'v4', http_auth)
-    result = sheets_service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
-    sheets = tuple(i['properties'] for i in result['sheets'])  # % sheets[0]['gridProperties']['rowCount']
     res = sheets_service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range="1:1").execute()
+
     values = res.get('values', [])
+    print(values)
     try:
         fields_count = len(values[0])
     except IndexError:
@@ -73,3 +81,43 @@ def email_test(request):
     # send_mail('Subject here', 'Here is the message.', settings.EMAIL_HOST_USER, ['tavito.286@gmail.com'],
     #           fail_silently=False)
     return render(request, 'home/dashboard.html', {'sheets': sheet_list})
+
+
+def async_spreadsheet_info(request, id):
+    storage = Storage('credentials')
+    credentials = storage.get()
+    http_auth = credentials.authorize(httplib2.Http())
+
+    sheets_service = discovery.build('sheets', 'v4', http_auth)
+
+    result = sheets_service.spreadsheets().get(spreadsheetId=id).execute()
+
+    sheets = tuple(i['properties'] for i in result['sheets'])  # % sheets[0]['gridProperties']['rowCount']
+
+    _sheets = [(i['index'], i['title']) for i in sheets]
+
+    request.session['google_sheets'] = _sheets
+
+    template = loader.get_template('home/_spreadsheet_sheets.html')
+    context = {'sheets': _sheets}
+
+    ctx = {'Success': True, 'sheets': template.render(context)}
+    return HttpResponse(json.dumps(ctx), content_type='application/json')
+
+
+def async_spreadsheet_values(request, id, sheet_id):
+    storage = Storage('credentials')
+    credentials = storage.get()
+    http_auth = credentials.authorize(httplib2.Http())
+
+    sheets_service = discovery.build('sheets', 'v4', http_auth)
+
+    sheets = request.session['google_sheets']
+
+    sheet_id = next((s[1] for s in sheets if s[0] == int(sheet_id)))
+
+    res = sheets_service.spreadsheets().values().get(spreadsheetId=id, range='{0}!A1:Z100'.format(sheet_id)).execute()
+    print(res)
+
+    ctx = {'Success': True}
+    return HttpResponse(json.dumps(ctx), content_type='application/json')
