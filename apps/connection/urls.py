68bbe0a5bedb2c9a104710d649d6f19fa5c476dc
Gustavo Saavedra <tavito.286@gmail.com>
2017-02-10 16:45:57 -0500

    Actualizacion del modulos connections

--- apps/connection/urls.py
+++ apps/connection/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 from apps.connection.views import CreateConnectionView, ListConnectionView, ListConnectorView, \
     AJAXFacebookGetAvailableConnectionsView, AJAXFacebookGetAvailableFormsView, AJAXFacebookGetAvailableLeadsView, \
     AJAXMySQLTestConnection, UpdateConnectionView, AJAXSugarCRMTestConnection, AJAXMailChimpTestConnection, \
-    GoogleAuthView, AjaxGoogleSpreadSheetTestConnection, GoogleAuthSuccessCreateConnection
+    GoogleAuthView, AjaxGoogleSpreadSheetTestConnection, GoogleAuthSuccessCreateConnection, \
+    AJAXPostgreSQLTestConnection, AJAXMSSQLTestConnection
 
 urlpatterns = [
     url(r'create/(?P<connector_id>\d+)/$', CreateConnectionView.as_view(), name='create'),
@@ -28,4 +29,8 @@ urlpatterns = [
         name='ajax_sugarcrm_test_connection'),
     url(r'ajax/mailchimp/test_connection/', AJAXMailChimpTestConnection.as_view(),
         name='ajax_mailchimp_test_connection'),
+    url(r'ajax/postgresql/test_connection/', AJAXPostgreSQLTestConnection.as_view(),
+        name='ajax_update_postgresql_test_connection'),
+    url(r'ajax/mssql/test_connection/', AJAXMSSQLTestConnection.as_view(),
+        name='ajax_update_mssql_test_connection'),
 ]
