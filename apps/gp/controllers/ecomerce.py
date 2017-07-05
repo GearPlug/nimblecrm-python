06387c1e03c24d54975bf457633bb76394643dd4
Lelia <lyuutachi@gmail.com>
2017-07-05 17:45:34 -0500

    Correcciones shopify

--- apps/gp/controllers/ecomerce.py
+++ apps/gp/controllers/ecomerce.py
@@ -118,36 +118,16 @@ class ShopifyController(BaseController):
             return [{"name":"first Name", "required":True, "type":'varchar'},
                     {"name":"last Name","required":False, "type":'varchar'},
                     {"name":"email","required":True, "type": 'varchar'},
-                    {"name":"company","required":False, "type": 'varchar'},
                     {"name":"address1", "required":False, "type": 'varchar'},
-                    {"name":"address2", "required":False, "type": 'varchar'},
                     {"name":"city","required":False, "type": 'varchar'},
-                    {"name":"province","required":False, "type": 'varchar'},
-                    {"name":"province code","required":False, "type": 'varchar'},
                     {"name":"country","required":False, "type": 'varchar'},
-                    {"name":"country code","requiered":False, "type": 'varchar'},
                     {"name":"zip","required":False, "type": 'varchar'},
                     {"name":"phone","required":True, "type": 'varchar'},
-                    {"name":"accepts marketing","required":True, "type": 'varchar', 'values':[True,False]},
-                    {"name":"total spent","required":False, "type": 'int'},
-                    {"name":"total orders","required":False, "type": 'int'},
-                    {"name":"tags","required":False, "type": 'varchar'},
-                    {"name":"note","required":False, "type": 'varchar'},
-                    {"name":"tax exempt","required":False, "type": 'int'},
                    ]
         if (topic_id=='products'):
-            return [{"name":"email", "required":True, "type":'varchar'},
-                    {"name":"accepts_marketing","required":False, "type":'varchar'},
-                    {"name":"first_name","required":True, "type": 'varchar'},
-                    {"name":"last_name","required":False, "type": 'varchar'},
-                    {"name":"orders_count", "required":False, "type": 'varchar'},
-                    {"name":"note", "required":False, "type": 'varchar'},
-                    {"name":"verified_email","required":False, "type": 'varchar'},
-                    {"name":"multipass_identifier","required":False, "type": 'varchar'},
-                    {"name":"tax_exempt","required":False, "type": 'varchar'},
-                    {"name":"phone","required":True, "type": 'varchar'},
-                    {"name":"tags","requiered":False, "type": 'varchar'},
-                    {"name":"last_order_name","required":False, "type": 'varchar'},
+            return [{"name":"title", "required":True, "type":'varchar'},
+                    {"name":"type","required":False, "type":'varchar'},
+                    {"name":"vendor","required":False, "type": 'varchar'},
                    ]
 
     def send_stored_data(self, source_data, target_fields, is_first=False):
@@ -159,9 +139,10 @@ class ShopifyController(BaseController):
             for item in data_list:
                 try:
                     if (topic_id=="customers"):
-                        response=self.insert_customers(data=data_list[0])
+                        response=self.create_customers(data=item)
+                    if (topic_id=="products"):
+                        response = self.create_product(data=item)
                     if response is True:
-                        print("item creado")
                         list = shopify.Customer.find()
                         id = list[-1]
                         id = re.findall(r'\d+', str(id))
@@ -172,48 +153,45 @@ class ShopifyController(BaseController):
                     print(e)
                     extra['status'] = 'f'
                     self._log.info('Item: failed to send.', extra=extra)
-                    return obj_list
+            return obj_list
         raise ControllerError("There's no plug")
 
     def get_mapping_fields(self):
         fields = self.get_fields()
         return [MapField(f, controller=ConnectorEnum.Shopify) for f in fields]
 
-    def insert_customers(self, data):  # Metodo para crear un nuevo producto
-        fields=self.get_fields()
-        values={}
-        for i in fields:
-            find=False
-            for d in data:
-                if (i['name']==d):
-                    values[i['name']]=data[d]
-                    find=True
-            if find is False:
-                values[i['name']]=''
+    def create_customers(self, data):
+        values=self.get_values(data)
         session = shopify.Session("https://" + settings.SHOPIFY_SHOP_URL + ".myshopify.com", self._token)
         shopify.ShopifyResource.activate_session(session)
         new_customer = shopify.Customer()
         new_customer.first_name = values["first Name"]
         new_customer.last_name = values["last Name"]
         new_customer.email = values["email"]
-        #new_customer.company = "compania"
+        new_customer.addresses = [{"address1": values["address1"], "city": values["city"], "phone": values["phone"]}]
         sucess=new_customer.save()
         return sucess
-        # new_customer.address1 = values["address1"]
-        # new_customer.address2 = values["address2"]
-        # new_customer.city = values["city"]
-        # new_customer.province = values["province"]
-        # new_customer.province_code= values["province code"]
-        # new_customer.country = values["country"]
-        # new_customer.country_code = values["country code"]
-        # new_customer.zip = values["zip"]
-        # new_customer.phone = values["phone"]
-        # new_customer.accepts_marketing = values["accepts marketing"]
-        # new_customer.total_spent = values["total spent"]
-        # new_customer.total_orders = values["total orders"]
-        # new_customer.tags = values["tags"]
-        # new_customer.note = values["note"]
-        # new_customer.tax_exempt = values["tax exempt"]
-        # if new_customer.error:
-        #     print(new_customer.error)
-        # return None
+
+    def create_product(self, data):
+        values = self.get_values(data)
+        session = shopify.Session("https://" + settings.SHOPIFY_SHOP_URL + ".myshopify.com", self._token)
+        shopify.ShopifyResource.activate_session(session)
+        new_product = shopify.Product()
+        new_product.title = values['title']
+        new_product.type = values['type']
+        new_product.vendor = values['vendor']
+        return new_product.save()
+
+    def get_values(self,data):
+        fields = self.get_fields()
+        values = {}
+        for i in fields:
+            find = False
+            for d in data:
+                if (i['name'] == d):
+                    values[i['name']] = data[d]
+                    find = True
+            if find is False: values[i['name']] = ""
+        return values
+
+
