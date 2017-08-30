de54d0d6498f57a2953e80019720e6dddae1239e
Gustavo Saavedra <tavito.286@gmail.com>
2017-08-30 17:01:07 -0500

    Dashboard y Mis gears backend

--- apps/gear/urls.py
+++ apps/gear/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from apps.gear.views import CreateGearView, CreateGearGroupView, UpdateGearView, DeleteGearView, ListGearView, \
-    CreateGearMapView, gear_toggle
+from apps.gear.views import CreateGearView, CreateGearGroupView, UpdateGearView, UpdateGearGroupView, DeleteGearView,\
+    ListGearView, CreateGearMapView, gear_toggle
 
 urlpatterns = [
     url(r'^create/$', CreateGearView.as_view(), name='create'),
     url(r'^group/create/$', CreateGearGroupView.as_view(), name='create_group'),
     url(r'^update/(?P<pk>\d+)/$', UpdateGearView.as_view(), name='update'),
+    url(r'^group/update/(?P<pk>\d+)/$', UpdateGearGroupView.as_view(), name='update_group'),
+
     url(r'^delete/(?P<pk>\d+)/$', DeleteGearView.as_view(), name='delete'),
     url(r'^list/$', ListGearView.as_view(), name='list'),
 
