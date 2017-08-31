bbaf8521c585d7364a7b85dfd54029d7db97b2ff
Gustavo Saavedra <tavito.286@gmail.com>
2017-08-31 15:57:30 -0500

    Cambios templates jueves

--- apps/plug/views.py
+++ apps/plug/views.py
@@ -3,10 +3,8 @@ from django.views.generic import CreateView, UpdateView, ListView, View, Templat
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
-from apps.plug.apps import APP_NAME as app_name
 from apps.gp.enum import ConnectorEnum
 from apps.gp.models import Gear, Plug, PlugActionSpecification, Action, ActionSpecification, Connection, StoredData
-from extra_views import ModelFormSetView
 import re
 
 
@@ -18,14 +16,14 @@ class CreatePlugView(LoginRequiredMixin, CreateView):
         - Calls the TestPlugView upon completion.
         - Uses ActionListView by AJAX.
         - Uses ActionSpecificationView by AJAX.
-
+        - Resets the connection_id stored in the session by the ConnectionListView.
 
         TODO
         - Revisar flujo y funcionalidad
         """
     model = Plug
     fields = ['connection', ]
-    template_name = 'wizard/plug_create.html'
+    template_name = 'plug/create.html'
     success_url = ''
     login_url = '/account/login/'
 
@@ -48,9 +46,8 @@ class CreatePlugView(LoginRequiredMixin, CreateView):
             elif self.object.plug_type == 'target':
                 g.target = self.object
             g.save()
-        except:
+        except Exception as e:
             print("There's no gear in session.")
-        print(self.request.POST)
         exp = re.compile('(^specification-)(\d+)')
         specification_list = [{'name': m.group(0), 'id': m.group(2), 'value': self.request.POST.get(m.group(0), None)}
                               for s in self.request.POST.keys() for m in [exp.search(s)] if m]
@@ -67,7 +64,6 @@ class CreatePlugView(LoginRequiredMixin, CreateView):
                          ConnectorEnum.Instagram, ConnectorEnum.YouTube, ConnectorEnum.Shopify,
                          ConnectorEnum.GoogleCalendar, ConnectorEnum.Asana, ConnectorEnum.Salesforce,
                          ConnectorEnum.Mandrill, ConnectorEnum.FacebookLeads, ConnectorEnum.Gmail]:
-                    print("WEBHOOK!!")
                     controller.create_webhook()
                 else:
                     controller.download_to_stored_data(self.object.connection.related_connection, self.object)
@@ -76,7 +72,7 @@ class CreatePlugView(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
-        return reverse('wizard:plug_test', kwargs={'pk': self.object.id})
+        return reverse('plug:test', kwargs={'pk': self.object.id})
 
 
 class ActionListView(LoginRequiredMixin, ListView):
@@ -84,25 +80,25 @@ class ActionListView(LoginRequiredMixin, ListView):
         Lists the Actions from a specific connector. Filters source or target actions as well.
 
         - Is used by the CreatePlugView in the wizard via AJAX.
+        - Uses the connection_id stored in the session by the ConnectionListView.
 
         TODO
         - Revisar flujo y funcionalidad
     """
     model = Action
-    template_name = 'wizard/action_list.html'
+    template_name = 'utils/select_options.html'
 
     def post(self, request, *args, **kwargs):
-        if not request.is_ajax():
-            return super(ActionListView, self).get(request, *args, **kwargs)
-        plug_type = request.POST.get('type', None)
+        return super(ActionListView, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        plug_type = self.request.POST.get('type', None)
         kw = {'action_type': plug_type}
+        connection_key = '{0}_connection_id'.format(plug_type)
         if plug_type in ['source', 'target']:
-            if '{0}_connection_id'.format(plug_type) in request.session:
-                kw['connector_id'] = Connection.objects.get(
-                    pk=request.session['{0}_connection_id'.format(plug_type)]).connector_id
-        self.object_list = self.model.objects.filter(**kw)
-        a = [{'name': a.name, 'id': a.id} for a in self.object_list]
-        return JsonResponse(a, safe=False)
+            if connection_key in self.request.session:
+                kw['connector_id'] = Connection.objects.get(pk=self.request.session[connection_key]).connector_id
+        return self.model.objects.filter(**kw)
 
 
 class ActionSpecificationsListView(LoginRequiredMixin, ListView):
@@ -117,32 +113,53 @@ class ActionSpecificationsListView(LoginRequiredMixin, ListView):
         - Revisar flujo y funcionalidad
     """
     model = ActionSpecification
-    template_name = 'wizard/async/action_specification.html'
+    template_name = 'plug/action_specifications.html'
 
     def get_context_data(self, **kwargs):
         context = super(ActionSpecificationsListView, self).get_context_data(**kwargs)
         return context
 
     def post(self, request, *args, **kwargs):
-        if not request.is_ajax():
-            return super(ActionSpecificationsListView, self).get(request, *args, **kwargs)
         action = Action.objects.get(pk=self.kwargs['action_id'])
-        kw = {'action_id': self.kwargs['action_id']}
-        self.object_list = self.model.objects.filter(**kw)
-        context = self.get_context_data()
         c = ConnectorEnum.get_connector(action.connector.id)
         if c in [ConnectorEnum.FacebookLeads, ConnectorEnum.GoogleSpreadSheets]:
-            self.template_name = 'wizard/async/action_specification/' + c.name.lower() + '.html'
+            self.template_name = 'plug/action_specifications/' + c.name.lower() + '.html'
+        return super(ActionSpecificationsListView, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return self.model.objects.filter(action_id=self.kwargs['action_id'])
+
+
+class PlugActionSpecificationListView(LoginRequiredMixin, TemplateView):
+    """
+
+    """
+    template_name = 'utils/select_options.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        connection_id = request.POST.get('connection_id', None)
+        action_specification_id = request.POST.get('action_specification_id', None)
+        connection = Connection.objects.get(pk=connection_id)
+        connector = ConnectorEnum.get_connector(connection.connector_id)
+        controller_class = ConnectorEnum.get_controller(connector)
+        controller = controller_class(connection.related_connection)
+        ping = controller.test_connection()
+        kwargs.update(
+            {key: val for key, val in request.POST.items() if key not in ['action_specification_id', 'connection_id']})
+        if ping:
+            field_list = controller.get_action_specification_options(action_specification_id, **kwargs)
         else:
-            self.template_name = 'wizard/async/action_specification.html'
-        return super(ActionSpecificationsListView, self).render_to_response(context)
+            field_list = []
+        context['object_list'] = field_list
+        return super(PlugActionSpecificationListView, self).render_to_response(context)
 
 
 class TestPlugView(TemplateView):
     """
 
     """
-    template_name = 'wizard/plug_test.html'
+    template_name = 'plug/test.html'
 
     def get_context_data(self, **kwargs):
         context = super(TestPlugView, self).get_context_data()
@@ -188,28 +205,3 @@ class TestPlugView(TemplateView):
                 data_list = controller.download_to_stored_data(p.connection.related_connection, p)
         context = self.get_context_data()
         return super(TestPlugView, self).render_to_response(context)
-
-
-class PlugActionSpecificationOptionsView(LoginRequiredMixin, TemplateView):
-    """
-
-    """
-    template_name = 'wizard/async/select_options.html'
-
-    def post(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        connection_id = request.POST.get('connection_id', None)
-        action_specification_id = request.POST.get('action_specification_id', None)
-        connection = Connection.objects.get(pk=connection_id)
-        connector = ConnectorEnum.get_connector(connection.connector_id)
-        controller_class = ConnectorEnum.get_controller(connector)
-        controller = controller_class(connection.related_connection)
-        ping = controller.test_connection()
-        kwargs.update(
-            {key: val for key, val in request.POST.items() if key not in ['action_specification_id', 'connection_id']})
-        if ping:
-            field_list = controller.get_action_specification_options(action_specification_id, **kwargs)
-        else:
-            field_list = []
-        context['object_list'] = field_list
-        return super(PlugActionSpecificationOptionsView, self).render_to_response(context)
