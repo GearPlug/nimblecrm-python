e7810f41a8ea886cb25b607d415c93b5041e363c
Luisa Torres <ltorres@grplu.com>
2017-09-04 16:58:09 -0500

    Activity

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^dashboard/$', DashBoardView.as_view(), name='dashboard'),
     url(r'^webhook/(?P<connector>(\S+))/(?P<webhook_id>(\S+))/$', IncomingWebhook.as_view(), name='webhook'),
     url(r'^apps/$', AppsView.as_view(), name='apps'),
-    url(r'^help/$', HelpView.as_view(), name='apps'),
-    url(r'^activity/$', ActivityView.as_view(), name='apps'),
+    url(r'^help/$', HelpView.as_view(), name='help'),
+    url(r'^activity/$', ActivityView.as_view(), name='activity'),
 ]
