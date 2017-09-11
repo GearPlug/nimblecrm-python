5aa5527d2cd8927c77ac0c93936054b9ceb843e7
Gustavo Saavedra <tavito.286@gmail.com>
2017-09-11 11:13:36 -0500

    Correccion de KWARGS en el controlador base

--- apps/gp/controllers/base.py
+++ apps/gp/controllers/base.py
@@ -1,6 +1,6 @@
 import logging
 from apps.gp.controllers.exception import ControllerError
-from apps.gp.models import Action, ActionSpecification
+from apps.gp.models import Connection
 
 logger = logging.getLogger('controller')
 
@@ -13,13 +13,13 @@ class BaseController(object):
     """
     _connection_object = None
     _plug = None
-    _log = logging.getLogger('controller')
     _connector = None
+    _log = logging.getLogger('controller')
 
     def __init__(self, *args, **kwargs):
         self.create_connection(*args, **kwargs)
 
-    def create_connection(self, *args):
+    def create_connection(self, connection=None, plug=None):
         """
         El args[0] debe ser la connection (especifica).
         El args[1] puede ser el Plug o None.
@@ -27,17 +27,14 @@ class BaseController(object):
         :param args:
         :return:
         """
-        if args:
-            self._connection_object = args[0]
-            try:
-                self._connector = self._connection_object.connector
-            except:
-                pass
-            try:
-                self._plug = args[1]
-            except:
-                pass
-            return
+        if connection:
+            if isinstance(connection, Connection):
+                connection = connection.related_connection
+            self._connection_object = connection
+            self._connector = self._connection_object.connector
+        if plug:
+            self._plug = plug
+        return
 
     def test_connection(self):
         raise ControllerError('Not implemented yet.')
@@ -68,5 +65,12 @@ class BaseController(object):
     def get_action_specification_options(self, action_specification_id):
         raise ControllerError('Not implemented yet.')
 
-    def do_webhook_process(self,**kwargs):
+    def create_webhook(self, **kwargs):
+        raise ControllerError('Webhooks are not supported.')
+
+    def do_webhook_process(self, **kwargs):
         raise ControllerError('Not implemented yet.')
+
+    @property
+    def connector(self):
+        return self._connector
