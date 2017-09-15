91bbb7f2cffac60e0e5e655ee932e5ad20959f37
Lelia <lyuutachi@gmail.com>
2017-09-15 16:00:48 -0500

    unitest googlesheets p2

--- tests/test_googlesheets.py
+++ tests/test_googlesheets.py
@@ -4,7 +4,10 @@ from apps.gp.models import Connection, ConnectorEnum, \
     PlugActionSpecification, \
     GoogleSpreadSheetsConnection
 from django.contrib.auth.models import User
+from datetime import datetime
 from django.test import TestCase, Client
+from apiclient import discovery
+import httplib2
 import json
 import os
 
@@ -90,7 +93,7 @@ class GoogleSpreadSheetsControllerTestCases(TestCase):
             'value': os.environ.get('worksheet')
         }
 
-        cls.plug_action_specificaion_source_1 = PlugActionSpecification.objects.create(
+        cls.plug_action_specificaion_source_2 = PlugActionSpecification.objects.create(
             **_dict6)
 
         _dict7 = {
@@ -99,7 +102,7 @@ class GoogleSpreadSheetsControllerTestCases(TestCase):
             'value': os.environ.get('sheet')
         }
 
-        cls.plug_action_specificaion_source_1 = PlugActionSpecification.objects.create(
+        cls.plug_action_specificaion_target_1 = PlugActionSpecification.objects.create(
             **_dict7)
 
         _dict8 = {
@@ -108,7 +111,7 @@ class GoogleSpreadSheetsControllerTestCases(TestCase):
             'value': os.environ.get('worksheet')
         }
 
-        cls.plug_action_specificaion_source_1 = PlugActionSpecification.objects.create(
+        cls.plug_action_specificaion_target_2 = PlugActionSpecification.objects.create(
             **_dict8)
 
     def setUp(self):
@@ -118,34 +121,80 @@ class GoogleSpreadSheetsControllerTestCases(TestCase):
             self.plug_target.connection.related_connection, self.plug_target)
         self._credentials= json.loads(os.environ.get('credentials'))
 
+    def _get_sheet_id(self):
+        id = ""
+        result = self.controller_source.get_sheet_list()
+        for i in result:
+            if i['name'] == self.controller_source._spreadsheet_id:
+                id = i['id']
+        return id
 
-    def test_controller(self):
-        self.assertIsInstance(self.controller_source._connection_object,
-                              GoogleSpreadSheetsConnection)
-        self.assertIsInstance(self.controller_source._plug, Plug)
-        self.assertIsInstance(self.controller_target._plug, Plug)
-        self.assertTrue(self.controller_source._credential)
-        self.assertTrue(self.controller_target._credential)
-        self.assertEqual(self.controller_source._spreadsheet_id, os.environ.get('sheet'))
-        self.assertEqual(self.controller_target._spreadsheet_id, os.environ.get('sheet'))
-        self.assertEqual(self.controller_source._worksheet_name, os.environ.get('worksheet'))
-        self.assertEqual(self.controller_target._worksheet_name, os.environ.get('worksheet'))
+    # def test_controller(self):
+    #     self.assertIsInstance(self.controller_source._connection_object,
+    #                           GoogleSpreadSheetsConnection)
+    #     self.assertIsInstance(self.controller_source._plug, Plug)
+    #     self.assertIsInstance(self.controller_target._plug, Plug)
+    #     self.assertTrue(self.controller_source._credential)
+    #     self.assertTrue(self.controller_target._credential)
+    #     self.assertEqual(self.controller_source._spreadsheet_id, os.environ.get('sheet'))
+    #     self.assertEqual(self.controller_target._spreadsheet_id, os.environ.get('sheet'))
+    #     self.assertEqual(self.controller_source._worksheet_name, os.environ.get('worksheet'))
+    #     self.assertEqual(self.controller_target._worksheet_name, os.environ.get('worksheet'))
+    #
+    # def test_test_connection(self):
+    #     result_source = self.controller_source.test_connection()
+    #     result_target = self.controller_source.test_connection()
+    #     self.assertTrue(result_source)
+    #     self.assertTrue(result_target)
 
-    def test_test_connection(self):
-        result_source = self.controller_source.test_connection()
-        result_target = self.controller_source.test_connection()
-        self.assertTrue(result_source)
-        self.assertTrue(result_target)
+    # def test_refresh_token(self):
+    #     field = GoogleSpreadSheetsConnection.objects.get(id=self.googlesheets_connection.id)
+    #     token = field.credentials_json["access_token"]
+    #     datetime_object = datetime.strptime('Jun 1 2005  1:33PM','%b %d %Y %I:%M%p')
+    #     self.controller_source._credential.token_expiry = datetime_object
+    #     self.controller_source._refresh_token()
+    #     field = GoogleSpreadSheetsConnection.objects.get(id=self.googlesheets_connection.id)
+    #     self.assertNotEqual(token,field.credentials_json["access_token"])
 
-    def test_refresh_token(self):
-        self._connection_object.credentials_json = self._credential.to_json()
-        self._connection_object.save()
+    # def test_column_string(self):
+    #     result = self.controller_source.colnum_string(10)
+    #     self.assertEqual(result, "J")
 
+    # def test_get_sheet_list(self):
+    #     result = self.controller_source.get_sheet_list()
+    #     sheet= ""
+    #     for i in result:
+    #         if i['name'] == os.environ.get('sheet'):
+    #             sheet=i['name']
+    #     self.assertEqual(sheet,os.environ.get('sheet'))
 
+    # def test_get_worksheet_list(self):
+    #     result = self.controller_source.get_sheet_list()
+    #     for i in result:
+    #         if i['name'] == os.environ.get('sheet'):
+    #             id = i['id']
+    #     result = self.controller_source.get_worksheet_list(id)
+    #     worksheet = ""
+    #     for i in result:
+    #         if i['title'] == os.environ.get('worksheet'):
+    #             worksheet = i['title']
+    #     self.assertEqual(worksheet, os.environ.get('worksheet'))
 
+    # def test_get_worksheet_values(self):
+    #     self.controller_source._spreadsheet_id=self._get_sheet_id()
+    #     result = self.controller_source.get_worksheet_values()
+    #     self.assertEqual(result[0][0], os.environ.get('first_field'))
+    #
+    # def test_get_worksheet
+    # _first_row(self):
+    #     self.controller_source._spreadsheet_id=self._get_sheet_id()
+    #     result = self.controller_source.get_worksheet_first_row()
+    #     self.assertEqual(result[0], os.environ.get('first_field'))
 
-
-
+    def test_create_row(self):
+        self.controller_source._spreadsheet_id = self._get_sheet_id()
+        print(self.controller_source._spreadsheet_id)
+        self.controller_target.create_row([["hola", "apellido", "nuevo", "nuevo3"], ["ff", "gg", "gg", "ll"]],3)
 
         # def test_get_target_fields(self):
         #     result=self.controller.get_target_fields()
