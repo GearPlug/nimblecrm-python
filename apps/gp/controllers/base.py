8cf292829ed7263e8a5d81ee294f8f6fef1528ff
Gustavo Saavedra <tavito.286@gmail.com>
2017-09-25 10:50:25 -0500

    agregado método send_target_data que se utilizara para enviar información al target.

--- apps/gp/controllers/base.py
+++ apps/gp/controllers/base.py
@@ -50,15 +50,19 @@ class BaseController(object):
     def download_source_data(self, **kwargs):
         if self._connection_object is not None and self._plug is not None:
             try:
-                return self.download_to_stored_data(self._connection_object,
-                                                    self._plug, **kwargs)
+                return self.download_to_stored_data(self._connection_object, self._plug, **kwargs)
             except TypeError:
-                print("CLASS TO FIX: {}".format(self))
-                return self.download_to_stored_data(self._connection_object,
-                                                    self._plug)
+                return self.download_to_stored_data(self._connection_object, self._plug)
 
         else:
             raise ControllerError("There's no active connection or plug.")
+
+    def send_target_data(self, *args, **kwargs):
+        if self._connection_object is not None and self._plug is not None:
+            try:
+                return self.send_stored_data(self._connection_object, self._plug, **kwargs)
+            except TypeError:
+                return self.send_stored_data(self._connection_object, self._plug)
 
     def get_target_fields(self, **kwargs):
         raise ControllerError('Not implemented yet.')
@@ -85,11 +89,10 @@ class GoogleBaseController(BaseController):
         self._connection_object.credentials_json = self._credential.to_json()
         self._connection_object.save(update_fields=['credentials_json'])
 
-    def _refresh_token(self, token=''):
+    def _refresh_token(self, **kwargs):
         if self._credential.access_token_expired:
             self._credential.refresh(httplib2.Http())
             self._upate_connection_object_credentials()
 
     def _report_broken_token(self, scale=None):
         print("IMPOSIBLE REFRESCAR EL TOKEN!!!! NOTIFICAR AL USUARIO.")
-
