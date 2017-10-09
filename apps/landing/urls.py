5ef7eae9ff2abd21b71b4a4b37613684b0b27b41
Miguel Ferrer <ingferrermiguel@gmail.com>
2017-10-09 15:06:21 -0500

    about us

--- apps/landing/urls.py
+++ apps/landing/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-from apps.landing.views import IndexView
+from apps.landing.views import IndexView, AboutUsView
+
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='indexpage'),
+    url(r'^aboutus/$', AboutUsView.as_view(), name='aboutus'),
 ]
--- apps/landing/views.py
+++ apps/landing/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'landing/index.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'landing/aboutus.html'
