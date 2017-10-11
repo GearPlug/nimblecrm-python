dda50c4143b4f92d367a19b1c65b5cc6669e7974
Gustavo Saavedra <tavito.286@gmail.com>
2017-10-11 17:17:01 -0500

    Cambios para enlace de landing. formulario de registro (falta ajax). Con mensajes de errores. Lista de top 6 apps. Pagina de aplicaciones.

--- apps/landing/urls.py
+++ apps/landing/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from apps.landing.views import IndexView, AboutUsView, ContactUsView, AppsView
+from apps.landing.views import IndexView, AboutUsView, ContactUsView, AppsView, CustomSignup
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^accounts/signup/$', CustomSignup.as_view(), name='signup'),
     url(r'^about/$', AboutUsView.as_view(), name='about'),
     url(r'^contact/$', ContactUsView.as_view(), name='contact'),
     url(r'^apps/directory/$', AppsView.as_view(), name='apps_directory'),
--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from allauth.account.views import SignupView
 from apps.landing.forms import NameSignupForm
 from apps.gp.models import Connector
 
@@ -8,7 +10,9 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['register_form'] = NameSignupForm()
+        context['signup_form'] = NameSignupForm()
+        context['top_apps'] = Connector.objects.filter(Q(name__in=['slack', 'gmail', 'mysql', 'facebookleads',
+                                                                   'sugarcrm', 'mailchimp']))
         return context
 
 
@@ -26,3 +30,14 @@ class AppsView(ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(is_active=True)
+
+
+class CustomSignup(SignupView, IndexView):
+    form_class = NameSignupForm
+    template_name = 'landing/index.html'
+
+    def __init__(self, *args, **kwargs):
+        return super(SignupView, self).__init__(*args, **kwargs)
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(signup_form=form))
