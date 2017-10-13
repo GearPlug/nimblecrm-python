0cfb949504b08438b3005f9b47417f8da7b3db5d
Nerio Rincon <nrincon.mr.j@gmail.com>
2017-10-13 09:55:41 -0500

    Formulario de registro en GearPlug con checkbox TOS y checkbox Suscripciones

--- apps/gp/forms.py
+++ apps/gp/forms.py
@@ -3,48 +3,24 @@ from django import forms
 from apps.gp.models import Subscriptions, SubscriptionsList
 
 class SignupForm(forms.ModelForm):
+    list = forms.ModelMultipleChoiceField(queryset=SubscriptionsList.objects.all(), widget=forms.CheckboxSelectMultiple)
 
     class Meta:
-        model = Subscriptions
+        model = SubscriptionsList
         fields = ('list',)
 
-    # query = SubscriptionsList.objects.values()
-    # for i in query:
-    #     print(i)
-    #     list = forms.ModelMultipleChoiceField(queryset=i ,widget=forms.CheckboxSelectMultiple)
-
-    # A custom method required to work with django-allauth, see
-    # https://stackoverflow.com/questions/12303478/how-to-customize-user-profile-when-using-django-allauth
     def signup(self, request, user):
-        print('1')
-        profile = Subscriptions()
-        profile.user = user
-        print(self.cleaned_data)
-        list_name = self.cleaned_data['list']
-        s_list = SubscriptionsList.objects.all().values()
-        for i in s_list:
-            if str(i['title']) == str(list_name):
-                profile.list_id = i['id']
-        profile.save()
+        selected_list = []
+        for i in self.cleaned_data['list']:
+            selected_list.append(i)
+            profile = Subscriptions()
 
-class OptionsForm(forms.Form):
-    OPTIONS = (
-        ("a", "A"),
-        ("b", "B"),
-    )
-    name = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                                     choices=OPTIONS)
-
-    def signup(self, request, user):
-        profile = Subscriptions()
-        profile.user = user
-        print('user name:', profile.user)
-        list_name = self.cleaned_data['list']
-        s_list = SubscriptionsList.objects.all().values()
-        print(s_list)
-        for i in s_list:
-            print('nombre de lista: {0}, titulo: {1}, id: {2}'.format(list_name, i['title'], i['id']))
-            if str(i['title']) == str(list_name):
-                profile.list_id = i['id']
-        print('list id:', profile.list_id)
-        profile.save()
+            profile.user = user
+            # for i in self.cleaned_data['list']:
+            #     selected_list.append(i)
+            for i in selected_list:
+                list = SubscriptionsList.objects.filter(title__exact=i).values()
+                for y in list.iterator():
+                    profile.list_id = y['id']
+                    print('list id: ', profile.list_id)
+                profile.save()
