27eac8969a0c9af95d2bbae2ab08fed18e8cd524
Nerio Rincon <nrincon.mr.j@gmail.com>
2017-11-07 10:27:50 -0500

    Modificaciones en client.py

--- nimblercrm/client.py
+++ nimblercrm/client.py
@@ -108,6 +108,7 @@ class Client(object):
         return ca.get_token(code=code)
 
     def token_expiration_checker(self):
+        print(self.top_time)
         if datetime.now() > self.top_time:
             self.to_refresh_token()
 
