9698ea4a3db1b32959acadae75e1ce22fb3ad036
Nerio Rincon <nrincon.mr.j@gmail.com>
2017-11-10 12:02:38 -0500

    Desglose de funcion get_contact_list para obtener los contactos divididos segun el tipo de contacto.

--- nimblercrm/client.py
+++ nimblercrm/client.py
@@ -57,6 +57,7 @@ class Client(object):
         }
 
         response = requests.request(method, url, headers=headers, data=data)
+        print(response.url)
         return (response)
 
     def _parse(self, response):
