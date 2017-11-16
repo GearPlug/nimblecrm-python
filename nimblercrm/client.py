f01f57bddaf12bf823e55df4aeb16295b7994c20
Nerio Rincon <nrincon.mr.j@gmail.com>
2017-11-16 18:46:03 -0500

    Actualizacion de metodos para que obtengan solo los actuales.

--- nimblercrm/client.py
+++ nimblercrm/client.py
@@ -111,7 +111,8 @@ class Client(object):
         return ca.get_token(code=code)
 
     def token_expiration_checker(self):
-        if datetime.now() > self.token_expiration_time:
+        dt = datetime.strptime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+        if dt > self.token_expiration_time:
             self.to_refresh_token()
         else:
             pass
