bac99c7bf7ffa0901ffc475237716d019100925b
Nerio Rincon <nrincon.mr.j@gmail.com>
2017-12-14 13:50:15 -0500

    Modificacion en clase GearActivitiesHistoryView para limitar y refinar el resultado del Queryset segun parametros indicados, ademas de eliminar el codigo que permitia y mostraba las opciones de busqueda.

--- apps/gear/views.py
+++ apps/gear/views.py
@@ -5,6 +5,7 @@ from django.views.generic import CreateView, UpdateView, DeleteView, ListView, F
 from django.views.generic.edit import FormMixin
 from django.http.response import JsonResponse, HttpResponseForbidden, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 from apps.gear.apps import APP_NAME as app_name
 from apps.gear.forms import MapForm, SendHistoryForm, DownloadHistoryForm, FiltersForm
 from apps.gp.enum import ConnectorEnum
@@ -14,7 +15,10 @@ from apps.history.models import DownloadHistory, SendHistory
 from oauth2client import client
 import httplib2
 import json
+import datetime
 from django.apps import apps
+from apiconnector import settings
+
 
 class ListGearView(LoginRequiredMixin, ListView):
     """
@@ -280,8 +284,9 @@ class ActivityView(LoginRequiredMixin, ListView):
     login_url = '/accounts/login/'
 
     def get_queryset(self):
+        print(1)
         gear_list = Gear.objects.filter(user=self.request.user)
-        recent_activity = self.model.objects.filter(gear_id__in=gear_list).order_by('-id')[:10]
+        recent_activity = self.model.objects.filter(gear_id__in=gear_list).order_by('date')[:30]
         obj_list = []
         for item in recent_activity:
             gear = gear_list.get(pk=item.gear_id)
@@ -289,6 +294,8 @@ class ActivityView(LoginRequiredMixin, ListView):
                              'target_connector': gear.target.connection.connector.name,
                              'action_source': gear.source.action.name,
                              'action_target': gear.target.action.name})
+        print(2, len(recent_activity))
+        print(3, len(obj_list))
         return obj_list
 
 
@@ -362,23 +369,13 @@ class GearActivitiesHistoryView(FormMixin, LoginRequiredMixin, ListView, ):
     login_url = '/accounts/login/'
 
     def get_queryset(self, **kwargs):
-        order = 'date'
-        if self.request.method == "POST":
-            if 'date_from' in self.request.POST and self.request.POST['date_from']:
-                if 'date_to' in self.request.POST and self.request.POST['date_to']:
-                    kwargs['date__range'] = (self.request.POST['date_from'], self.request.POST['date_to'])
-                else:
-                    kwargs['date__gte'] = self.request.POST['date_from']
-            elif 'date_to' in self.request.POST and self.request.POST['date_to']:
-                kwargs['date__lte'] = self.request.POST['date_to']
-            if 'order' in self.request.POST and self.request.POST['order'] == 'desc':
-                order = '-date'
-            if 'sent' in kwargs and kwargs['sent'] == '0':
-                del kwargs['sent']
-            del kwargs['date_from']
-            del kwargs['date_to']
-            del kwargs['order']
         gears = list(Gear.objects.filter(user_id=self.request.user.id).values_list('id', flat=True))
+        today_min = datetime.datetime.combine(timezone.now().date(), datetime.time.min)
+        if settings.USE_TZ:
+            today_min = timezone.make_aware(today_min, timezone.get_current_timezone())
+        today_max = datetime.datetime.combine(timezone.now().date(), datetime.time.max)
+        if settings.USE_TZ:
+            today_max = timezone.make_aware(today_max, timezone.get_current_timezone())
         return [{'connection': json.loads(item.connection)[0]['fields']['name'],
                  'data': [{'name': k, 'value': v} for k, v in json.loads(item.data).items()],
                  'date': item.date,
@@ -388,7 +385,9 @@ class GearActivitiesHistoryView(FormMixin, LoginRequiredMixin, ListView, ):
                  'response': item.response,
                  'identifier': item.identifier,
                  'id': item.id,
-                 } for item in self.model.objects.filter(gear_id__in=gears, **kwargs).order_by(order)]
+                 } for item in self.model.objects.filter(gear_id__in=gears,
+                                                         date__range=(today_min,today_max),
+                                                         **kwargs).order_by('-date')[:30]]
 
     def get(self, request, *args, **kwargs):
         try:
