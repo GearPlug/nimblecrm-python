f6cb70f9984d2474699a5998f6097ca764f03150
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-09-20 13:58:28 -0500

    Release stable version 0.1.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ NimbleCRM API wrapper written in python.
 
 ## Installing
 ```
-pip install git+git://github.com/GearPlug/nimblecrm-python.git
+pip install nimblecrm-python
 ```
 
 ## Requirements
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup
 
-setup(name='nimblercrm',
-      version='0.1',
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='nimblecrm-python',
+      version='0.1.0',
       description='API wrapper for NimblerCRM written in Python',
+      long_description=read('README.md'),
       url='https://github.com/GearPlug/nimblecrm-python',
       author='Nerio Rincon',
       author_email='nrincon.mr@gmail.com',
